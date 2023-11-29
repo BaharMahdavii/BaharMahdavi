e4bb43d0ffc0c908963e61d876705304c93560db
BaharMahdavii <152422410+BaharMahdavii@users.noreply.github.com>
2023-11-29 20:07:45 +0330

    Add files via upload

---
+++ assignment2-2.py
@@ -0,0 +1,7 @@
+a=int(input("enter a:"))
+b=int(input("enter b:"))
+c=int(input("enter c:"))
+if a+b>c and a+c>b and b+c>a:
+    print("it can be created")
+else:
+    print("it can not be created")    
